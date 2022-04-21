cccc6e1bd2773b0bcd8686537dfb4a90e964c1bf
Oleg Frolov <oleg_frolov@mail.ru>
2022-04-21 15:19:56 +0300

    update

--- functions.php
+++ functions.php
@@ -1,1 +1,20 @@
-/home/www/wb3/modules/yonger/common/scripts/functions.php
+<?php
+setlocale(LC_ALL, 'ru_RU.utf8');
+
+function datetext($date)
+{
+    return strftime('%e %B %Y', strtotime($date));
+}
+
+function dateform($date)
+{
+    return date('d.m.Y', strtotime($date));
+}
+
+function text2tel($str) {
+    return preg_replace("/\D/", '', $str);
+}
+
+@include_once(__DIR__ . '/engine/modules/yonger/common/scripts/functions.php');
+
+?>
