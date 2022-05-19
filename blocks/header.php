79efd83d482272691297b986b80e4af26926e7d6
Oleg Frolov <oleg_frolov@mail.ru>
2022-05-19 14:21:40 +0300

    update

--- blocks/header.php
+++ blocks/header.php
@@ -30,7 +30,8 @@
                             <use xlink:href="/assets/img/sprites/svgsprites.svg#drop"></use>
                         </svg>
                         <div class="enter__panel">
-                            <a class="enter__btn text-small" href="/lk/{{_sess.user.role}}/profile">Редактировать</a>
+                            <a class="enter__btn text-small" href="/lk/{{_sess.user.role}}/profile">Мой профиль</a>
+                            <a class="enter__btn text-small" href="/lk/{{_sess.user.role}}">Мой кабинет</a>
                             <a class="enter__btn text-small" href="/signout">Выйти</a>
                         </div>
                     </button>
