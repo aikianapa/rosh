1def392ef05b89f3996aab66d7829da5f3aad019
HubArtWork <68958381+HubArtWork@users.noreply.github.com>
2022-05-27 10:20:06 +0300

    add post page

--- assets/js/new.js
+++ assets/js/new.js
@@ -1,16 +1,46 @@
 $(document).ready(function(){
-    var swiper = new Swiper('.product-slider', {
-      slidesPerView: 1,
-      watchSlidesVisibility: true,
-      watchOverflow: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-    //card
-    $('.card-bottom .btn').on('click', function(e){
-        e.preventDefault();
-        $(this).parents('.card-bottom').toggleClass('active');
-    });
+
+  //header
+  var header = $('.header'),
+    scrollPrev = 0;
+
+  $(window).scroll(function() {
+    var scrolled = $(window).scrollTop();
+   
+    if ( scrolled > 10 && scrolled > scrollPrev ) {
+      header.addClass('out');
+    } else {
+      header.removeClass('out');
+    }
+    scrollPrev = scrolled;
+  });
+
+  //header flip
+  if($('.header__logo').hasClass('header__logo-red')){
+    setTimeout(function(){
+      $('.header__logo-red').addClass('active');
+    }, 2000);
+  }
+
+  //sliders
+  var swiper = new Swiper('.product-slider', {
+    slidesPerView: 1,
+    watchSlidesVisibility: true,
+    watchOverflow: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  var swiper = new Swiper('.post-slider', {
+    slidesPerView: 1,
+    watchOverflow: true,
+    spaceBetween: 30,
+  });
+
+  //card
+  $('.card-bottom .btn').on('click', function(e){
+      e.preventDefault();
+      $(this).parents('.card-bottom').toggleClass('active');
+  });
 });
