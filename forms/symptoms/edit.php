0a9bb2da08220ebe65eda62c1e9a95914fa80e64
Oleg Frolov <oleg_frolov@mail.ru>
2022-08-18 23:12:59 +0300

    symptoms

--- forms/symptoms/edit.php
+++ forms/symptoms/edit.php
@@ -1,6 +1,6 @@
 <html>
 <div class="modal effect-scale show removable" id="modalPagesEdit" data-backdrop="static" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-xxl" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header row">
                 <div class="col-5">
@@ -13,10 +13,10 @@
             </div>
             <div class="modal-body pd-20">
                 <div class="row">
-                    <div class="col-5 scroll-y modal-h">
+                    <div class="col-12">
                         <form id="{{_form}}EditForm">
                             <div class="form-group">
-                                <wb-module wb="module=filepicker&mode=single&width=800&height=300" wb-path="/uploads/services/" name="cover">
+                                <wb-module wb="module=filepicker&mode=single&width=400&height=200" wb-path="/uploads/services/" name="cover">
                                 </wb-module>
                             </div>
 
@@ -27,10 +27,17 @@
                                             <input name="active" wb-module="swico">
                                         </span>
                                     </div>
-                                    <textarea name="header" class="form-control" placeholder="Наименование симптома" required></textarea>
+                                    <input type="text" name="header" class="form-control" placeholder="Наименование симптома" required />
                                 </div>
                             </div>
 
+                            <div class="form-group row">
+                                    <div class="col">
+                                    <label class="col-form-label">Описание</label>
+                                    <textarea name="text" class="form-control" placeholder="Описание"></textarea>
+                                    </div>
+                                </div>
+                            
                             <div class="form-group row">
                                 <label class="col-12">Связанные проблемы</label>
                                 <div class="col-12">
@@ -52,17 +59,9 @@
 
                             <input type="hidden" name="category" value="">
 
-                            <wb-module wb="module=yonger&mode=structure&preset=landing" />
                         </form>
                     </div>
 
-                    <div class="col-7 scroll-y modal-h">
-                        <div id="yongerBlocksForm">
-                            <form method="post">
-
-                            </form>
-                        </div>
-                    </div>
                 </div>
             </div>
             <div class="modal-footer pd-x-20 pd-b-20 pd-t-0 bd-t-0">
@@ -72,52 +71,6 @@
     </div>
 </div>
 
-<div class="modal effect-slide-in-right left w-50" id="modalPagesEditBlocks" data-backdrop="true" tabindex="-1">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <i></i>
-                <i class="fa fa-close cursor-pointer" data-dismiss="modal" aria-label="Close"></i>
-            </div>
-            <div class="modal-body p-0 pb-5 scroll-y">
-                <div class="list-group" id="{{_form}}EditFormListBlocks">
-                    <wb-foreach wb="from=_null&render=client&bind=yonger.blocks">
-                        <a class="list-group-item list-group-item-action" href="javascript:void(0)" data-name="{{name}}" onclick="yonger.yongerPageBlockAdd('{{id}}')">
-                            <span>{{name}}</span>
-                            <span class="d-block tx-11 text-muted">{{header}}</span>
-                        </a>
-                    </wb-foreach>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<script wb-app>
-    yonger.pageEditor = function() {
-        let $form = $('#{{_form}}EditForm');
-        $form.delegate('[name=path]', 'change', function() {
-            let path = $(this).val() + '/';
-            $form.find('.path').html(path);
-            $form.find('[name=name]').trigger('change');
-        });
-        $form.delegate('[name=name]', 'change keyup', function() {
-            let path = $form.find('[name=path]').val() + '/';
-            let name = $(this).val();
-            if (path == '/' && name == 'home') name = '';
-            $form.find('.path').html(path + name);
-        });
-        $form.find('[name=path]').trigger('change');
-
-        $form.find('.pagelink').on(wbapp.evClick, function() {
-            let url = $(this).text();
-            let target = md5(url);
-            window.open(url, target).focus();
-        })
-    }
-
-    yonger.pageEditor();
-</script>
 <wb-lang>
     [ru]
     main = Основное
