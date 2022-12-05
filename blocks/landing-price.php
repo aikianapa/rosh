bb072cd5bbf9ec906777b1a358a51ea24a30ea7a
Oleg Frolov <oleg_frolov@mail.ru>
2022-12-05 13:11:14 +0300

    update

--- blocks/landing-price.php
+++ blocks/landing-price.php
@@ -56,8 +56,8 @@
     <div class="landing page">
         <div class="container">
             <div class="row">
-                <div class="col-md-2"></div>
-                <div class="col-md-9">
+                <div class="col-md-4"></div>
+                <div class="col-md-6">
                     <div class="content-price">
                         <div class="content-price__head">
                             <p>Консультация врача</p>
