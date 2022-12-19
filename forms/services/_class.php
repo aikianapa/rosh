f3172539f78c9aeee3279d6959d61a3c22b39d73
Oleg Frolov <oleg_frolov@mail.ru>
2022-12-19 09:19:55 +0300

    services list sort

--- forms/services/_class.php
+++ forms/services/_class.php
@@ -11,12 +11,21 @@ class servicesClass extends cmsFormsClass
         }
     }
 
-
-    function afterItemSave($item)
+    function sort()
     {
-        if ($this->app->route->mode == 'save') {
-            $this->app->shadow('/cms/ajax/form/pages/list');
+        $data = $this->app->vars('_post');
+        $res = ['error'=>true];
+        foreach ($data as $sort => $item) {
+            $this->app->itemSave($this->app->route->form, [
+                    'id'=>$item,
+                    '_sort' => wbSortIndex($sort)
+                ], false);
+            $res = ['error'=>false];
         }
+        $this->app->tableFlush($this->app->route->form);
+        header("Content-type:application/json");
+        echo json_encode($res);
+        exit;
     }
 
 
---
+++ forms/services/list.js
@@ -0,0 +1,107 @@
+(() => {
+    var api = "/api/v2"
+    var form = "services"
+    var size = 99999
+    var base = api + `/list/${form}?&@size=${size}&@sort=_sort`
+    var list = new Ractive({
+        el: `#${form}List`,
+        template: $(`#${form}List`).html(),
+        data: {
+            base: base,
+            result: [],
+            pagination: [],
+            user: wbapp._session.user,
+            filter: false
+        },
+        on: {
+            init() {
+                let base = this.get("base");
+                wbapp.post(`${base}`, {}, function(data) {
+                    list.fire("setData", null, data)
+                })
+            },
+            setData(ev, data) {
+                list.set("result", data.result);
+                list.set("pagination", data.pagination);
+                list.set("page", data.page);
+                list.set("pages", data.pages);
+                if (list.get('filter') == false) {
+                    $(`#${form}List .list-group`).sortable({
+                        update: function(ev, line) {
+                            let data = {}
+                            $(ev.target).children().each(function(i, li) {
+                                data[i] = $(li).data('id')
+                            })
+                            wbapp.post(`/api/v2/func/${form}/sort`, data)
+                        }
+                    });
+                } else {
+                    $(`#${form}List .list-group`).sortable("destroy");
+                }
+            },
+            setPage(ev) {
+                let page = $(ev.node).attr("data-page");
+                this.fire("loadPage", page)
+                return false
+            },
+            switch (ev) {
+                let id = $(ev.node).parents('[data-id]').attr('data-id');
+                let active = '';
+                ev.get('active') == 'on' ? active = '' : active = 'on';
+                wbapp.post(`/api/v2/update/${form}/` + id, {
+                    active: active
+                }, function(res) {
+                    if (res.active !== undefined) ev.set('active', res.active)
+                })
+            },
+            filter(ev) {
+                let base = this.get("base");
+                let filter = {}
+                $(list.el).find('.search :input').each(function() {
+                    if ($(this).attr('name') == 'filter') {
+                        let val = $(this).val()
+                        if (val > '') {
+                            filter['$or'] = [
+                                { header: { '$like': val } },
+                                //{ email: { '$like': val } },
+                                //{ phone: { '$like': val } }
+                            ]
+                        }
+                    }
+                    if ($(this).attr('name') == 'category') {
+                        let category = $(this).val()
+                        category == '*' ? null : filter.category = { '$like': category }
+                    }
+                })
+                Object.keys(filter).length == 0 ? list.set('filter', false) : list.set('filter', true)
+                wbapp.post(`${base}`, { filter: filter }, function(data) {
+                    list.fire("setData", null, data)
+                })
+            },
+            remove(ev) {
+                let id = $(ev.node).parents('[data-id]').attr('data-id');
+                let result = list.get("result")
+                let page = list.get("page") * 1
+                let pages = list.get("pages") * 1
+                delete result[id]
+                if (Object.keys(result).length == 0 && pages > 0 && page >= pages) page--
+                    wbapp.post(`${api}/delete/${form}/${id}`, {}, function(data) {
+                        if (data.error == false) {
+                            list.fire("loadPage", page)
+                        }
+                    });
+            },
+            loadPage(ev, page) {
+                wbapp.post(`${base}&@page=${page}`, {}, function(data) {
+                    list.fire("setData", null, data)
+                })
+            }
+        }
+    })
+    $(document).undelegate(`#${form}EditForm`, 'wb-form-save');
+    $(document).delegate(`#${form}EditForm`, 'wb-form-save', function(ev, res) {
+        if (res.params.item !== undefined && res.data !== undefined) {
+            list.set("result." + res.data.id, res.data);
+        }
+    })
+})($)
