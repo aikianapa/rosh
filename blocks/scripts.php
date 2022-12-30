85818e5896ef143ecbbae33733cb649d7aba40c1
O.T <oleg.tyran@gmail.com>
2022-12-30 13:15:30 +0200

    + upd. fast quote form

--- blocks/scripts.php
+++ blocks/scripts.php
@@ -40,7 +40,7 @@
 	</script>
 
 	<script wb-app wb-if="in_array('{{_sess.user.role}}',['main','client','expert'])"
-		src="/assets/js/cabinet.js?v=F76927FB6F274701BD75F894156EE6D6">
+		src="/assets/js/cabinet.js?v=90173C46C1BE419A9011FE014D2ED229">
 	</script>
 
 	<div wb-if="'{{_sett.devmode}}!='on'">
