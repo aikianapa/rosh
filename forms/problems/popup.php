9898e2c386f271a72b286c8bbf8056bba61d6186
Oleg Frolov <oleg_frolov@mail.ru>
2023-02-03 12:52:38 +0300

    update

--- forms/problems/popup.php
+++ forms/problems/popup.php
@@ -1,11 +1,14 @@
 <html>
 
-<h2 class="h2 mb-20">{{header}}</h2>
+<h2 class="mb-20 h2">{{header}}</h2>
 <div class="popup__img"><img src="/thumbc/750x287/src{{cover.0.img}}" alt=""></div>
-
-<p class="text-bold text-big mb-10" wb-if="'{{blocks.landing_main.active}}' == 'on'">{{blocks.landing_main.subheader}}</p>
-<div class="text text-break" wb-if="'{{blocks.landing_main.active}}' == 'on'">{{blocks.landing_main.text}}</div>
-
+<wb-var text="" />
+<wb-foreach wb="from=blocks&tpl=false&parent=false" wb-filter="active=on&text>=">
+    <div class="text text-break" wb-if="'{{_var.text}}' == '' && '{{trim(strip_tags({{text}}))}}'>'a'">
+        <wb-var text="{{text}}" />
+        {{wbGetWords({{_var.text}},150)}}
+    </div>
+</wb-foreach>
 <a class="btn btn--black" href="{{yongerFurl()}}">Читать подробнее</a>
 
 </html>
