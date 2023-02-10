6d371499f48c2bcfaa978567970090f40c73ca62
o.tyran <oleg.tyran@gmail.com>
2023-02-10 05:10:34 +0200

    + updates

--- blocks/header.php
+++ blocks/header.php
@@ -94,11 +94,39 @@
 		            </a>
 	            </div>
             </div>
+	        <div wb-if="'{{_sess.user.role}}'=='main'">
+		        <div wb-if="'{{_route.uri}}' =='/english'">
+			        <wb-var hover_logo="{{is_hover_logo({{_route.uri}})}}"></wb-var>
 
-	        <div class="container --flex --jcsb --aicn" wb-if="'{{_sess.user.role}}'=='main'">
-		        <div class="header__admin --flex --aicn" wb-if="'{{_route.uri}}' !=='/english'">
-			        <span class="lower-deck">
-				        <button class="btn btn--white loaddata --openpopup d-none d-lg-block"
+			        <a wb-if="'{{_var.hover_logo}}' == '0'" class="header__logo" href="/">
+				        <img src="/assets/img/logo.svg" alt="">
+				        <img src="/assets/img/logo-red.svg" alt="">
+			        </a>
+
+			        <a wb-if="'{{_var.hover_logo}}' == '1'" class="header__logo header__logo-red" href="/">
+				        <div class="header__logo-wrap">
+					        <div class="logo"><img src="/assets/img/logo.svg" alt=""></div>
+					        <div class="logo-red"><img src="/assets/img/logo-red.svg" alt=""></div>
+				        </div>
+			        </a>
+		        </div>
+		        <div class="container --flex --jcsb --aicn">
+			        <div class="header__admin --flex --aicn" wb-if="'{{_route.uri}}' !=='/english'">
+				        <span class="lower-deck">
+					        <button class="btn btn--white loaddata --openpopup d-none d-lg-block"
+						        onclick="popupDownloadData();"
+						        data-popup="--download-data">
+						        <svg class="svgsprite _xl">
+							        <use xlink:href="/assets/img/sprites/svgsprites.svg#xl"></use>
+						        </svg>
+						        Выгрузить данные
+					        </button>
+				        </span>
+				        <a class="btn btn-link" href="/cabinet">Заявки и события</a>
+				        <a class="btn btn-link" href="/cabinet/search">Пациенты</a>
+				        <a class="btn btn-link" href="/cabinet/photos">Медиатека</a>
+				        <a class="btn btn-link" href="/cabinet/changes">Журнал изменений</a>
+				        <button class="btn btn--white loaddata --openpopup d-sm-block d-lg-none d-md-none"
 					        onclick="popupDownloadData();"
 					        data-popup="--download-data">
 					        <svg class="svgsprite _xl">
@@ -106,53 +134,66 @@
 					        </svg>
 					        Выгрузить данные
 				        </button>
-			        </span>
-			        <a class="btn btn-link" href="/cabinet">Заявки и события</a>
-			        <a class="btn btn-link" href="/cabinet/search">Пациенты</a>
-			        <a class="btn btn-link" href="/cabinet/photos">Медиатека</a>
-			        <a class="btn btn-link" href="/cabinet/changes">Журнал изменений</a>
-			        <button class="btn btn--white loaddata --openpopup d-sm-block d-lg-none d-md-none"
-				        onclick="popupDownloadData();"
-				        data-popup="--download-data">
-				        <svg class="svgsprite _xl">
-					        <use xlink:href="/assets/img/sprites/svgsprites.svg#xl"></use>
-				        </svg>
-				        Выгрузить данные
-			        </button>
-		        </div>
-		        <div class="header__right --flex --aicn" wb-if="'{{_route.uri}}' !=='/english'">
-			        <button class="btn btn-link profile-menu">
-				        Профиль
-				        <svg class="svgsprite _drop">
-					        <use xlink:href="/assets/img/sprites/svgsprites.svg#drop"></use>
-				        </svg>
-				        <div class="enter__panel">
-					        <a class="enter__btn text-small" onclick="popupEditProfile();">Редактировать</a>
-					        <a class="enter__btn text-small signout" href="/signout">Выйти</a>
+			        </div>
+			        <div class="header__right --flex --aicn" wb-if="'{{_route.uri}}' !=='/english'">
+				        <button class="btn btn-link profile-menu">
+					        Профиль
+					        <svg class="svgsprite _drop">
+						        <use xlink:href="/assets/img/sprites/svgsprites.svg#drop"></use>
+					        </svg>
+					        <div class="enter__panel">
+						        <a class="enter__btn text-small" onclick="popupEditProfile();">Редактировать</a>
+						        <a class="enter__btn text-small signout" href="/signout">Выйти</a>
+					        </div>
+				        </button>
+				        <button class="burger"></button>
+			        </div>
+
+			        <div class="header__left --flex --aicn" wb-if="'{{_route.uri}}' =='/english'">
+				        <wb-var tel="+7{{_var.cityPrefix}}{{_var.cityPhone}}"/>
+				        <wb-var tel='{{str_replace("+78","+7",{{_var.tel}})}}'/>
+				        <div class="header__contacts"><a class="header__contact" href="tel:+{{text2tel({{_var.tel}})}}">
+						        {{_var.cityPrefix}} <b>{{_var.cityPhone}}</b></a>
+					        <div wb-if="'{{_route.uri}}' !=='/english'" class="header__contact header__contact--small">{{_var.worktime}}</div>
+					        <wb-data wb="table=pages&item={{_route.item}}&field=blocks.contacts_english" wb-if="'{{_route.uri}}' =='/english'">
+						        <div class="header__contact header__contact--small" wb-if="'{{worktime}}'>''">{{worktime}}</div>
+					        </wb-data>
 				        </div>
-			        </button>
-                    <button class="burger"></button>
-                </div>
-            </div>
-            <div class="crumbs__en" wb-if="'{{_route.uri}}'=='/english'">
-                <div class="container">
-                    <div class="crumbs"><a class="crumbs__arrow" href="javascript:window.history.back();">
-                            <svg class="svgsprite _crumbs-back-white">
-                                <use xlink:href="assets/img/sprites/svgsprites.svg#crumbs-back-white"></use>
-                            </svg></a><a class="crumbs__link" href="/">Home page</a>
-                            <span class="crumbs__link">Contacts</span></div>
-                </div>
-            </div>
+			        </div>
+			        <div class="header__right --flex --aicn" wb-if="'{{_route.uri}}' =='/english'">
+				        <a class="en-version pt-0" href="/">
+					        <svg class="svgsprite _web">
+						        <use xlink:href="/assets/img/sprites/svgsprites.svg#web"></use>
+					        </svg>
+					        Русская версия
+					        <svg class="svgsprite _arrow-link">
+						        <use xlink:href="/assets/img/sprites/svgsprites.svg#arrow-link"></use>
+					        </svg>
+				        </a>
+			        </div>
+		        </div>
+	        </div>
+	        <div class="crumbs__en" wb-if="'{{_route.uri}}'=='/english'">
+		        <div class="container">
+			        <div class="crumbs"><a class="crumbs__arrow" href="javascript:window.history.back();">
+					        <svg class="svgsprite _crumbs-back-white">
+						        <use xlink:href="assets/img/sprites/svgsprites.svg#crumbs-back-white"></use>
+					        </svg>
+				        </a><a class="crumbs__link" href="/">Home page</a>
+				        <span class="crumbs__link">Contacts</span>
+			        </div>
+		        </div>
+	        </div>
         </header>
     </div>
 </view>
 <edit header="Заголовок сайта">
-    <div>
-        <wb-module wb="module=yonger&mode=edit&block=common.inc" />
-    </div>
-    <wb-multilang wb-lang="{{_sett.locales}}" name="lang">
-        <div class="form-group">
-            <div class="col-12">
+	<div>
+		<wb-module wb="module=yonger&mode=edit&block=common.inc"/>
+	</div>
+	<wb-multilang wb-lang="{{_sett.locales}}" name="lang">
+		<div class="form-group">
+			<div class="col-12">
                 <label class="form-control-label">{{_lang.name}}</label>
                 <input type="text" class="form-control" name="header" placeholder="{{_lang.name}}">
             </div>
