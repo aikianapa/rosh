3788a1dbd1131aab8fdfbe9021485be8fe0b6dcb
Oleg Frolov <oleg_frolov@mail.ru>
2023-02-23 17:43:46 +0300

    update

--- blocks/services.php
+++ blocks/services.php
@@ -8,12 +8,12 @@
         `);
 		$(".crumbs + div.title-flex + h1").prependTo($(".crumbs + div.title-flex"));
 
-		$(".all-tabs-item.data-tab-link[data-tab]").on('click', function (ev) {
+		$(".all-tabs-item.data-tab-link[data-tab]").on('click', function(ev) {
 			$(".search.search--service .search__input").trigger("keyup");
 			let category = $(this).data('tab');
 			wbapp.storage('services.list.category', category);
-			console.log(category);
-			if (category == undefined || category == '') {
+			//console.log(category);
+			if (category == undefined || category == '' || category == 'all') {
 				$(".all-tabs .data-tab-item[data-tab]").removeClass('active');
 				$(".all-tabs .data-tab-item[data-tab=all]").addClass('active');
 				$(".all-services .all-services__item[data-category]").show();
@@ -25,11 +25,11 @@
 			}
 		});
 
-		$(".search.search--service .search__input").on("keyup", function () {
+		$(".search.search--service .search__input").on("keyup", function() {
 			let regex = $(this).val();
 			if (regex > ' ') {
 				regex = new RegExp(regex, "gi");
-				$(".all-services .all-services__item").each(function () {
+				$(".all-services .all-services__item").each(function() {
 					let str = $(this).text();
 					str.match(regex) ? $(this).show() : $(this).hide();
 				});
@@ -78,7 +78,7 @@
                             'active':'on'
                         }
                     }">
-						<wb-var image="{{cover.0.img}}" wb-if="'{{cover.0.img}}'>''" else="/assets/img/all/1.jpg"/>
+						<wb-var image="{{cover.0.img}}" wb-if="'{{cover.0.img}}'>''" else="/assets/img/all/1.jpg" />
 						<a class="all-services__item" href="{{yongerFurl()}}" data-category="{{category}}">
 							<div class="all-services__pic" style="background-image: url(/thumbc/510x314/src{{_var.image}})"></div>
 							<div class="all-services__name" wb-if="'{{header}}'>''">{{header}}</div>
@@ -87,7 +87,7 @@
 				</div>
 			</div>
 			<div class="all-tab data-tab-item" data-tab="lab" id="priceListLab">
-				<wb-var pricelist wb-api="/api/v2/list/price?active=on&@return=id,header,price,category"/>
+				<wb-var pricelist wb-api="/api/v2/list/price?active=on&@return=id,header,price,category" />
 				<div class="row">
 					<div class="col-lg-8">
 						<div class="service" wb-tree="dict=shop_category&branch=lab&parent=false">
@@ -101,13 +101,7 @@
 												<div class="service__price" wb-if="'{{price}}'>''">{{price}} ₽</div>
 												<div class="service__checkbox">
 													<div class="checkbox">
-														<input type="checkbox"
-															wb-if="'{{price}}'>''"
-															data-id="{{id}}"
-															data-service_price="{{category}}-{{id}}"
-															data-category="{{category}}"
-															data-price="{{price}}"
-															data-name="{{header}}" on-click="cartAdd">
+														<input type="checkbox" wb-if="'{{price}}'>''" data-id="{{id}}" data-service_price="{{category}}-{{id}}" data-category="{{category}}" data-price="{{price}}" data-name="{{header}}" on-click="cartAdd">
 														<span></span>
 													</div>
 												</div>
@@ -139,18 +133,17 @@
 
 									</div>
 									<div class="input">
-										<input class="input__control" type="text" value="{{user.fullname}}"
-											name="fullname" placeholder="ФИО" required>
+										<input class="input__control" type="text" value="{{user.fullname}}" name="fullname" placeholder="ФИО" required>
 										<div class="input__placeholder">ФИО</div>
 									</div>
 									<div class="input">
-										<input class="input__control intl-tel" value="{{user.phone}}"
-											name="phone" type="tel" required>
+										<input class="input__control intl-tel" value="{{user.phone}}" name="phone" type="tel" required>
 										<div class="input__placeholder active">Номер телефона</div>
 									</div>
 									<button class="mb-20 all-form__submit btn btn--black" on-click="submit">Записаться</button>
 									<div class="form__description">Нажимая на кнопку "Записаться", Вы даете согласие на обработку своих персональных данных на основании
-										<a href="policy.html">Политики конфиденциальности</a></div>
+										<a href="policy.html">Политики конфиденциальности</a>
+									</div>
 									<div class="form-bottom">После отправки заявки для Вас будет создан Личный кабинет, в&nbsp;который можно попасть через кнопку «Войти» в&nbsp;верхнем меню сайта</div>
 								</div>
 							</form>
@@ -162,7 +155,7 @@
 		</div>
 	</div>
 	<script wbapp>
-		$(document).on('wb-ready wb-ajax-done', function () {
+		$(document).on('wb-ready wb-ajax-done', function() {
 			window.fastQuoteServiceCreate = new Ractive({
 				el: '.fastquote-block',
 				template: wbapp.tpl('#fastquote-block-services').html,
@@ -172,7 +165,7 @@
 				on: {
 					complete() {
 						var _self = this;
-						setTimeout(function () {
+						setTimeout(function() {
 							initPlugins($(_self.el));
 						}, 500);
 					},
@@ -185,23 +178,23 @@
 								console.log('try to createProfile: ', post);
 
 								let names = post.fullname.split(' ', 3);
-								let keys  = ['last_name', 'first_name', 'middle_name'];
+								let keys = ['last_name', 'first_name', 'middle_name'];
 								for (var i = 0; i < names.length; i++) {
 									post[keys[i]] = names[i];
 								}
 								var _token = wbapp._settings.devmode === 'on' ? '123' : wbapp._session.token;
 								post.phone = str_replace([' ', '-', '(', ')'], '', post.phone);
 								window.api.get('/api/v2/list/users/?role=client&phone=' + post.phone +
-								               '&__token=' + _token).then(
-									function (data) {
+									'&__token=' + _token).then(
+									function(data) {
 										if (!data.length) {
-											post.role      = "client";
-											post.role      = "client";
+											post.role = "client";
+											post.role = "client";
 											post.confirmed = 0;
-											post.active    = "on";
-											post.__token   = _token;
+											post.active = "on";
+											post.__token = _token;
 											window.api.post('/api/v2/create/users/', post).then(
-												function (data) {
+												function(data) {
 													if (data && data.error) {
 														wbapp.trigger('wb-save-error', {
 															'data': data
@@ -223,7 +216,7 @@
 					}
 				}
 			});
-			var createFastQuoteServices   = function (client_id, data) {
+			var createFastQuoteServices = function(client_id, data) {
 				function unique(array) {
 					function onlyUnique(value, index, self) {
 						return self.indexOf(value) === index;
@@ -232,45 +225,49 @@
 					return array.filter(onlyUnique);
 				}
 
-				var quote        = {};
-				quote.group      = 'quotes';
-				quote.status     = 'new';
+				var quote = {};
+				quote.group = 'quotes';
+				quote.status = 'new';
 				quote.pay_status = 'unpay';
-				quote.client     = client_id;
-				quote.priority   = 0;
-				quote.marked     = false;
+				quote.client = client_id;
+				quote.priority = 0;
+				quote.marked = false;
 
-				quote.comment        = '';
+				quote.comment = '';
 				quote.recommendation = '';
-				quote.description    = '';
+				quote.description = '';
 				quote.client_comment = '';
 
-				quote.price          = data?.price || 0;
-				quote.services       = Object.values(data?.services || {});
+				quote.price = data?.price || 0;
+				quote.services = Object.values(data?.services || {});
 				quote.service_prices = data?.service_prices;
 
-				quote.event_date       = '';
-				quote.event_time       = '';
+				quote.event_date = '';
+				quote.event_time = '';
 				quote.event_time_start = '';
-				quote.event_time_end   = '';
+				quote.event_time_end = '';
 
-				quote.for_consultation  = 0;
+				quote.for_consultation = 0;
 				quote.longterm_date_end = '';
-				quote.longterm_title    = '';
+				quote.longterm_title = '';
 
-				quote.photos  = {before: [], after: []};
+				quote.photos = {
+					before: [],
+					after: []
+				};
 				quote.__token = wbapp._settings.devmode === 'on' ? '123' : wbapp._session.token;
 				console.log('Save new quote record:', quote);
 				window.api.post(
 					'/api/v2/create/records/', quote).then(
-					function (data) {
+					function(data) {
 						if (data.error) {
-							wbapp.trigger('wb-save-error',
-								{'data': data});
+							wbapp.trigger('wb-save-error', {
+								'data': data
+							});
 						} else {
 							popupMessage('Заявка создана!', 'Мы перезвоним Вам в ближайшее время!',
 								'', '',
-								function (d) {});
+								function(d) {});
 							$('#priceListLab .col-lg-8').addClass('col-lg-12').removeClass('col-lg-8');
 							$('.fastquote-block .all-form__succed').removeClass('d-none');
 							$('.fastquote-block .all-form__succed').show();
@@ -280,12 +277,11 @@
 
 			};
 		});
-
 	</script>
 </view>
 
 <edit header="Все услуги">
 	<div>
-		<wb-module wb="module=yonger&mode=edit&block=common.inc"/>
+		<wb-module wb="module=yonger&mode=edit&block=common.inc" />
 	</div>
 </edit>
