bdb9a28a2d36e357f4e294ccf27e8a811b90aed8
Олег Фролов <oleg_frolov@mail.ru>
2023-06-12 10:47:13 +0300

    block page-menu

---
+++ blocks/page-menu.php
@@ -0,0 +1,55 @@
+<view>
+    <div>
+        <wb-foreach wb="from=pagemenu&tpl=false">
+            <div><a href="{{link}}">{{label}}</a></div>
+        </wb-foreach>
+    </div>
+</view>
+
+<edit header="Меню блоков">
+    <div>
+        <wb-module wb="module=yonger&mode=edit&block=common.inc" />
+    </div>
+    <wb-var pagemenu="{{wbNewId()}}" />
+    <div id="{{_var.pagemenu}}">
+        <wb-multiinput name="pagemenu">
+            <input name="label" placeholder="Метка">
+            <div class="dropdown">
+                <input name="link" placeholder="Ссылка" on-click="dropdown" data-toggle="dropdown" aria-expanded="false">
+                <div class="dropdown-menu" wb-off>
+                    {{#each blocks}}
+                        {{#if active == 'on'}}
+                            {{#if name !== 'page-menu'}}
+                                <div class="dropdown-item" data-id="blk{{id}}" on-click="select">
+                                    <div>{{header}}</div>
+                                    <div class="lh-5 tx-gray tx-normal tx-11">{{name}}</div>
+                                </div>
+                            {{/if}}
+                        {{/if}}
+                    {{/each}}
+                </div>
+            </div>
+        </wb-multiinput>
+        <div>
+
+        </div>
+    </div>
+    <script>
+        var pagemenu = new Ractive({
+            el: `#{{_var.pagemenu}}`,
+            template: $(`#{{_var.pagemenu}}`).html(),
+            data: {
+                blocks: []
+            },
+            on: {
+                dropdown() {
+                    this.set('blocks', ypbrBlocks.get('blocks'));
+                },
+                select(ev) {
+                    $(ev.node).closest('.dropdown').children('input')[0].value = '#' + ev.node.dataset.id
+                    $(ev.node).trigger('change')
+                }
+            }
+        })
+    </script>
+</edit>
--- forms/problems/list.js
+++ forms/problems/list.js
@@ -2,7 +2,7 @@
     var api = "/api/v2"
     var form = "problems"
     var size = 50
-    var base = api + `/list/${form}?&@size=${size}&@sort=header`
+    var base = api + `/list/${form}?&@size=${size}&@sort=header&@return=id,header,_sort`
     var list = new Ractive({
         el: `#${form}List`,
         template: $(`#${form}List`).html(),
