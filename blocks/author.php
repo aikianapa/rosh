407fdf6b4733e74107b6a555864aaa708d820d01
Олег Фролов <oleg_frolov@mail.ru>
2023-06-27 13:49:32 +0300

    block author

--- blocks/author.php
+++ blocks/author.php
@@ -2,19 +2,7 @@
     <div>
         <wb-module wb="module=yonger&mode=edit&block=common.inc" />
     </div>
-    <div class="form-group row">
-        <label class="col-form-label col-sm-3">Подзаголовок</label>
-        <div class="col-sm-9">
-            <input name="subheader" class="form-control tx-bold">
-        </div>
-    </div>
-    <div class="form-group row">
-        <label class="col-form-label col-12">Текст цитаты</label>
-        <div class="col-sm-12">
-            <textarea name="text" class="form-control" rows="auto"></textarea>
 
-        </div>
-    </div>
     <div class="form-group row">
         <label class="col-form-label col-12">Автор</label>
         <div class="col-sm-12">
@@ -31,16 +19,12 @@
 
 <view>
     <div class="landing">
-        <div class="container info">
+        <div class="container author">
             <div class="row --aicn mb-80">
                 <div class="col-lg-4">
-                    <div class="aside">
-                        <h3 class="mb-40 h3" wb-if="'{{subheader}}'>''">{{subheader}}</h3>
-                    </div>
                 </div>
                 <div class="col-lg-6">
                     <blockquote>
-                        <p wb-if="'{{text}}'>''">«{{text}}»</p>
                         <figcaption>
                             <span wb-if="'{{author}}'>''">{{author}}</span>
                             <cite wb-if="'{{subtitle}}'>''">{{subtitle}}</cite>
