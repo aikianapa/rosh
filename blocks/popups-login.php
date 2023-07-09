08d9116d7eebf040de4dde4edb3c29f7ae0f90d8
Олег Фролов <oleg_frolov@mail.ru>
2023-07-09 20:57:16 +0300

    authPhone by enter

--- blocks/popups-login.php
+++ blocks/popups-login.php
@@ -13,7 +13,7 @@
 						</button>
 						<div class="popup__name text-bold">Войдите или зарегистрируйтесь</div>
 
-						<form class="popup__form">
+						<form class="popup__form" on-submit="submit">
 
 							<div class="input input--grey">
 								<input class="input__control intl-tel" name="phone" type="tel" autocomplete="off">
@@ -21,13 +21,13 @@
 								<span class="phone-error-msg">Неправильный номер телефона</span>
 							</div>
 
-							<div class="alert alert-warning mb-2"></div>
+							<div class="mb-2 alert alert-warning"></div>
 
 							<a class="form__link email_window_show" href="javascript:void(0)">Войти по почте</a>
+							<div class="form-bottom">
+								<button class="btn btn--black form__submit" disabled>Получить код</button>
+							</div>
 						</form>
-						<div class="form-bottom">
-							<button class="btn btn--black form__submit" disabled>Получить код</button>
-						</div>
 					</div>
 				</div>
 			</div>
@@ -43,7 +43,7 @@
 							</svg>
 						</button>
 						<div class="popup__name text-bold">Вход</div>
-						<form class="popup__form code_panel" >
+						<form class="popup__form code_panel">
 							<h3 class="h3">Введите код</h3>
 							<div class="form-title__description">
 								Мы отправили код подтверждения на номер
@@ -64,7 +64,7 @@
 								<input class="code__input" type="text">
 							</div>
 
-							<div class="alert alert-warning mb-2"></div>
+							<div class="mb-2 alert alert-warning"></div>
 						</form>
 					</div>
 				</div>
@@ -86,7 +86,7 @@
 								<input class="input__control" name="login" type="email" placeholder="Ваш е-мейл">
 								<div class="input__placeholder">Ваш е-мейл</div>
 							</div>
-							<div class="text-right mb-10">
+							<div class="mb-10 text-right">
 								<a class="form__link" href="javascript:void(0)" onclick="auth.recover_window_show()">Забыли пароль?</a>
 							</div>
 							<div class="input input--grey">
@@ -94,7 +94,7 @@
 								<div class="input__placeholder">Пароль</div>
 							</div>
 
-							<div class="alert alert-warning mb-2"></div>
+							<div class="mb-2 alert alert-warning"></div>
 
 							<a class="form__link" type="button" href="javascript:void(0)" onclick="auth.phone_window_show()">Войти по номеру телефона</a>
 							<div class="form-bottom --flex">
@@ -124,7 +124,7 @@
 								<div class="input__placeholder">Ваш е-мейл</div>
 							</div>
 
-							<div class="alert alert-warning mb-2"></div>
+							<div class="mb-2 alert alert-warning"></div>
 
 							<a class="form__link" type="button" href="javascript:void(0)" onclick="auth.phone_window_show()"> Войти по номеру телефона</a>
 							<div class="form-bottom --flex">
@@ -152,15 +152,16 @@
 								<div class="input__placeholder">Ваш е-мейл</div>
 							</div>
 
-							<div class="alert alert-warning mb-2"></div>
+							<div class="mb-2 alert alert-warning"></div>
 
 							<div class="form__alert text-small email_is_busy">Адрес уже используется, хотите&nbsp;<a class="link link--medium" href="javascript:void(0)" onclick="auth.recover_window_show()">восстановить пароль</a>?
-                            <svg class="svgsprite _alert">
-	                            <use xlink:href="/assets/img/sprites/svgsprites.svg#alert"></use>
-                            </svg>
+								<svg class="svgsprite _alert">
+									<use xlink:href="/assets/img/sprites/svgsprites.svg#alert"></use>
+								</svg>
 							</div>
 							<div class="form__description">Нажимая на кнопку "Зарегистрироваться", Вы даете согласие на обработку своих персональных данных на основании
-								<a href="/policy">Политики конфиденциальности</a></div>
+								<a href="/policy">Политики конфиденциальности</a>
+							</div>
 							<div class="form-bottom --flex">
 								<a class="btn btn--black do_register" type="button">Зарегистрироваться</a>
 								<a class="btn btn--white" type="button" href="javascript:void(0)" onclick="auth.email_enter_window_show()">Войти</a>
@@ -172,14 +173,21 @@
 		</template>
 	</div>
 
-	<script wbapp >
-		(function () {
+	<script wbapp>
+		(function() {
 			new Ractive({
 				el: '#popupsLogin',
 				template: document.querySelector('#popupsLogin template').innerHTML,
 				data: {},
+				on: {
+					submit(el) {
+						el.event.preventDefault()
+						el.event.stopPropagation()
+						$(el.node).find('button.form__submit').trigger('click')
+					}
+				}
 			})
 		})()
-    </script>
+	</script>
 
 </view>
