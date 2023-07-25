35c23f9f9f8c6c3d0ca3ae1f41375fcf3e1704ba
Олег Фролов <oleg_frolov@mail.ru>
2023-07-25 12:23:51 +0300

    update

--- tpl/cabinet.php
+++ tpl/cabinet.php
@@ -1,2 +1,5 @@
+<script>
+    if ('{{_sess.user.role}}' == 'admin') document.location.href = '/workspace'
+</script>
 <wb-include wb-tpl="cabinet/{{_sess.user.role}}{{_route.val}}.php" wb-if="in_array('{{_sess.user.role}}',['main','client','expert'])"></wb-include>
 <wb-include wb-tpl="404.php" wb-if="in_array('{{_sess.user.role}}',['admin',''])"></wb-include>
