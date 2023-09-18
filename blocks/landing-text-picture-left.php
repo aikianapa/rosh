959a46f3e2519db9c1dd4e9f78e93d55f4c99658
Dmitriy Sarankov <95976121+VikdiMarks@users.noreply.github.com>
2023-09-18 08:33:46 +0300

    blog text wrapping

--- blocks/landing-text-picture-left.php
+++ blocks/landing-text-picture-left.php
@@ -3,9 +3,11 @@
         <div class="post__block left">
             <div class="post__block-info text">
                 <span class="post__block-title" wb-if="'{{title}}'>''">{{title}}</span>
-                <p wb-if="'{{text}}'>''">{{nl2br(text)}}</p>
+                <p wb-if="'{{text}}'>''">
+                    <div class="post__block-img" wb-if="'{{image.0.img}}'>''"><img src="{{image.0.img}}" alt="{{title}}"></div>
+                    {{nl2br(text)}}
+                </p>
             </div>
-            <div class="post__block-img" wb-if="'{{image.0.img}}'>''"><img src="{{image.0.img}}" alt="{{title}}"></div>
         </div>
     </div>
 </view>
