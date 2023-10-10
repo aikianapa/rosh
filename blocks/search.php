b3344858fa9ae1af8c681c46e25e2719dfe6a381
VikdiMarks <sarankov.dima@gmail.com>
2023-10-10 22:13:18 +0300

    Revert "fix search"
    This reverts commit 3e848225d2ded467ef275b957964eeaecaac6a61.

--- blocks/search.php
+++ blocks/search.php
@@ -1,51 +1,45 @@
 <view>
-    <main class="page {{_session.user.role}}" data-barba="container" data-barba-namespace="lk-cabinet">
-
-        <div class="account admin">
-            <div class="container">
-                <template>
-                    <div class="crumbs">
-                        <a class="crumbs__arrow" href="#">
-                            <svg class="svgsprite _crumbs-back">
-                                <use xlink:href="/assets/img/sprites/svgsprites.svg#crumbs-back"></use>
-                            </svg>
-                        </a>
-                        <a class="crumbs__link" href="/">Главная</a>
-                        <a class="crumbs__link" href="/search">Поиск</a>
-                    </div>
-                    <div class="title-flex --flex --jcsb">
-                        <div class="title">
-                            <h1 class="mb-10 h1">Поиск</h1>
-                        </div>
-                    </div>
-                    <div class="search__block --flex --aicn">
-                        <div class="input">
-                            <input class="search__input" type="text" name="q" placeholder="Поиск"
-                                   value="{{searchValue}}" on-keyup="search">
-                        </div>
-                        <button class="btn btn--black" on-click="submit">Найти</button>
-                    </div>
-
-                    <div class="search-result" wb-off>
-                        <div class="container">
-                            <div class="data-tab-item" data-tab="all">
-                                <div class="all-services" id="servicesList">
-                                    {{#res}}
-                                    <a class="all-services__item" href="{{url}}" data-category="{{category}}">
-                                        <div class="all-services__pic"
-                                             style="background-image: url('{{cover[0].img || image[0].img}}')"></div>
-                                        <div class="all-services__name">{{header}}</div>
-                                    </a>
-                                    {{/res}}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </template>
+    <div class="container">
+        <template>
+            <div class="crumbs">
+                <a class="crumbs__arrow" href="#">
+                    <svg class="svgsprite _crumbs-back">
+                        <use xlink:href="/assets/img/sprites/svgsprites.svg#crumbs-back"></use>
+                    </svg>
+                </a>
+                <a class="crumbs__link" href="/">Главная</a>
+                <a class="crumbs__link" href="/search">Поиск</a>
             </div>
-        </div>
+            <div class="title-flex --flex --jcsb">
+                <div class="title">
+                    <h1 class="mb-10 h1">Поиск</h1>
+                </div>
+            </div>
+            <div class="search__block --flex --aicn">
+                <div class="input">
+                    <input class="search__input" type="text" name="q" placeholder="Поиск"
+                           value="{{searchValue}}" on-keyup="search">
+                </div>
+                <button class="btn btn--black" on-click="submit">Найти</button>
+            </div>
 
-    </main>
+            <div class="search-result" wb-off>
+                <div class="container">
+                    <div class="data-tab-item" data-tab="all">
+                        <div class="all-services" id="servicesList">
+                            {{#res}}
+                            <a class="all-services__item" href="{{url}}" data-category="{{category}}">
+                                <div class="all-services__pic"
+                                     style="background-image: url('{{cover[0].img || image[0].img}}')"></div>
+                                <div class="all-services__name">{{header}}</div>
+                            </a>
+                            {{/res}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </template>
+    </div>
 
     <script wbapp>
         window.search = () => {
