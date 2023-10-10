0b1cf21c2293db491be2fbba4d348ecad2dc6335
Олег Фролов <oleg_frolov@mail.ru>
2023-10-10 22:19:59 +0300

    gitpull

--- gitpull.php
+++ gitpull.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
-exec('/var/www/rosh.dev && git pull origin dev -a', $res);
+exec('/var/www/rosh.dev && git pull --no-edit origin dev ', $res);
 echo $res;
 ?>
