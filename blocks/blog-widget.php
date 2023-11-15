929e9ca92199fa6473b5e577356618ef88ce1217
VikdiMarks <sarankov.dima@gmail.com>
2023-11-15 23:09:15 +0300

    fix block questions icons

--- blocks/blog-widget.php
+++ blocks/blog-widget.php
@@ -1,6 +1,6 @@
 <view>
     <div class="blogs" data-blog-id="">
-        <div class="container" style="padding:0!important;">
+        <div class="container">
             <div class="blogs__list" id="blogList">
                 <wb-var matrix="{{get_blog_matrix()}}"></wb-var>
 
